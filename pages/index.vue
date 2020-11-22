cf1164c7696767eee70346e04357ab80e4a05f04
Ebuka Ezeh <ebukaloper@gmail.com>
2020-11-22 18:56:25 +0100

    feat: Initialize landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,117 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">splashilo</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div class="search">
+      <div class="search__container">
+        <IconSearch class="search__container-icon" />
+        <input
+          type="text"
+          name=""
+          placeholder="Search for photo"
+          class="search__container-input"
+        />
+      </div>
+      <div class="search__results">
+        <div class="search__results-item variant__two"></div>
+        <div class="search__results-item variant__one"></div>
+        <div class="search__results-item variant__three"></div>
+        <div class="search__results-item variant__two"></div>
+        <div class="search__results-item variant__one"></div>
+        <div class="search__results-item variant__three"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import IconSearch from '@/components/icons/IconSearch'
+
+export default {
+  components: {
+    IconSearch,
+  },
+}
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
-  min-height: 100vh;
+  // min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.search {
+  background-color: $navy-3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 5rem;
+  width: 50%;
+  border-radius: 0.1rem;
+  position: relative;
+  max-width: 1140px;
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  &__container {
+    width: 100%;
+    position: relative;
 
-.links {
-  padding-top: 15px;
+    &-icon {
+      width: 1.2rem;
+      height: 1.2rem;
+      position: absolute;
+      left: 1.4rem;
+      top: 0.8rem;
+      fill: $navy;
+    }
+
+    &-input {
+      padding: 1rem 4rem;
+      width: 100%;
+      border: 0;
+      border-radius: 0.2rem;
+      color: $navy;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
+  &__results {
+    margin-top: 4rem;
+    position: absolute;
+    top: 5rem;
+    width: 100%;
+    column-gap: 30px;
+    padding: 0 9rem;
+
+    @media only screen and (min-width: 992px) {
+      column-count: 3;
+    }
+
+    &-item {
+      margin: 0 0 30px;
+      display: inline-block;
+      width: 100%;
+      border-radius: 0.3rem;
+
+      &.variant__one {
+        height: 250px;
+        background-color: $gray-2;
+      }
+
+      &.variant__two {
+        height: 225px;
+        background-color: $gray-2;
+      }
+
+      &.variant__three {
+        height: 300px;
+        background-color: $gray-2;
+      }
+    }
+  }
 }
 </style>
