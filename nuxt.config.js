c9882b0c593ffd887c4326d063365a1e7f5ba1d7
Ebuka Ezeh <ebukaloper@gmail.com>
2020-11-22 18:54:11 +0100

    add: SCSS setup and files

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,22 @@ export default {
     title: 'splashilo',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+      },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    '@/assets/scss/variables.scss',
+    '@/assets/scss/styles.scss',
+    '@/assets/scss/typography.scss',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -25,6 +33,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/style-resources',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -33,9 +42,15 @@ export default {
     '@nuxtjs/axios',
   ],
 
+  styleResources: {
+    scss: ['./assets/scss/variables.scss'],
+  },
+
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    extractCSS: true,
+  },
 }
