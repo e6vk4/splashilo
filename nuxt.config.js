272800720a9c6a7a624560a4cecc26187632d036
Ebuka Ezeh <ebukaloper@gmail.com>
2020-11-23 10:59:23 +0100

    add: Dotenv package

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     '@nuxtjs/style-resources',
+    '@nuxtjs/dotenv',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
